1508dd23a622ced08098fa5a8e100f1c0d2316ce
juanvalencia <juanvalencia19410@gmail.com>
2025-11-12 16:31:53 -0500

    intento 3

--- backend/src/config.js
+++ backend/src/config.js
@@ -11,10 +11,4 @@ module.exports = {
         url: process.env.SUPABASE_URL,
         anonKey: process.env.SUPABASE_ANON_KEY,
     },
-    mysql: {
-        host: process.env.MYSQL_HOST || 'localhost',
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || '',
-        database: process.env.MYSQL_DB || 'ejemplo',
-    }
 }
