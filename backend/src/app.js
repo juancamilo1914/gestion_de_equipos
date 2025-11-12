0908376ac84c2845cb9f02b84bdb07223ce910f4
juanvalencia <juanvalencia19410@gmail.com>
2025-11-12 16:45:52 -0500

    intento 4

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://gestion-de-equipos.vercel.app" }));
 
 
 //config
